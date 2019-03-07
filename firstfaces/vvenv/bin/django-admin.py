b46be937d5330d958002b755ee57e6d9087e6199
John <you@example.com>
2019-03-07 17:35:41 +0000

    checking out why tests is wrong and transcripts not recording

---
+++ firstfaces/vvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/john/johnsHDD/PhD/2018_autumn/firstfacesProduction/firstfaces/vvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ firstfaces/vvenv/lib/python3.6/_dummy_thread.py
@@ -0,0 +1,1 @@
+/home/john/anaconda3/lib/python3.6/_dummy_thread.py
