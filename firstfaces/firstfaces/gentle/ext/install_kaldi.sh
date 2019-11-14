0b42e99ae657a10020fcd6367a7c01cd7ebcd42d
John <you@example.com>
2019-11-14 09:51:55 +0000

    fixed length of sentence bug

---
+++ firstfaces/firstfaces/gentle/ext/install_kaldi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prepare Kaldi
+cd kaldi/tools
+make clean
+make src/Makefile
+./extras/install_openblas.sh
+cd ../src
+# make clean (sometimes helpful after upgrading upstream?)
+./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
+make depend
+make
+cd ../../
