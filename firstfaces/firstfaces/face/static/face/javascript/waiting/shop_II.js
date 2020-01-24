dab023a1d2bdcd99c0568a258aa0feea745832bf
Danmaguire <daniel.maguire@ucdconnect.ie>
2020-01-24 16:10:05 +0000

    Gifs added 2

---
+++ firstfaces/firstfaces/face/static/face/javascript/waiting/shop_II.js
@@ -0,0 +1,70 @@
+
+function flash_eyes(time=300, wait=0, st_callback=function(){}){
+    start_hex = decimal_rgb_hex(tiaObject['mEyeL'+waitingVariables.attributes.eyes].material[0].color)
+    setTimeout(function(){
+        animate_eye_colour(start_hex, "FFFFFF",time);
+        setTimeout(function(){
+            animate_eye_colour('FFFFFF', start_hex,time);
+            setTimeout(function(){
+                st_callback();
+            },time);
+        },time);
+
+    },wait);
+}
+
+function animate_eye_colour(start_hex, end_hex, time){
+    var colours = get_transition_colors(start_hex, end_hex, time)
+    change_eye_color_with_delay(0,colours,colours.length);
+}
+function change_eye_color_with_delay(i,colours,end){
+    setTimeout(function(){
+        change_eye(colours[i])
+        i++;
+        if(i < end){
+            change_eye_color_with_delay(i,colours,end);
+        }
+    },16.666666667);
+}
+
+
+function fadeBackground(time=300, wait=0, st_callback=function(){}, reverse=false){
+        intervals = Math.round(time/16.666666667)-1;
+        var colour =  new THREE.Color(parseInt('0x'+decimal_rgb_hex(renderer.getClearColor()),16));
+        diff = 1/intervals;
+        vals = [1];
+        for(i=1;i<intervals;i++){
+            vals.push(vals[i-1]-diff);
+        }
+        console.log(vals);
+        vals.push(0);
+        if(reverse){
+            vals = vals.reverse()
+        }
+        fade_background_with_delay(0, vals, colour, vals.length);
+}
+function fade_background_with_delay(i, vals, hex, end){
+    setTimeout(function(){
+        renderer.setClearColor( hex , vals[i]);
+        i++;
+        if(i < end){
+            fade_background_with_delay(i, vals, hex, end);
+        }
+    },16.666666667);
+}
+
+var gif_prefix = prefixURL+"media/gifs/";
+
+function set_background_as_gif(gif,time=300){
+    $('#gifHolder').hide().attr("src",gif_prefix+gif).fadeIn(time);
+}
+function switch_to_gif(gif,time=300){
+    fadeBackground(time);
+    set_background_as_gif(gif,time=time)
+}
+
+function flash_gif(){
+}
+function transition_gif_gif(){
+}
+
